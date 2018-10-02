4c2eccb1f0f8d49967e6d7a231879bfa8757af77
LAPTOP-6L7QMV7L\浪子尘心 <536418286@qq.com>
2018-10-03 00:50:43 +0800

    修改一个bug

--- deploy_vimIDE.sh
+++ deploy_vimIDE.sh
@@ -14,12 +14,12 @@
 [ -f ~/.vimrc ] && mv ~/.vimrc  ~/.vimrc.bak
 [ -d ~/.vim ] && mv ~/.vim ~/.vim_bak
 # 配置vim
-mv vimIDE_config/vimrc_pymode.sh ~/.vimrc.bundles
+mv vimIDE_config/vimrc_plug.sh ~/.vimrc.bundles
 mkdir -p ~/.vim/vim_header_model
 mv vimIDE_config/vim_header.sh ~/.vim/vim_header_model/vim_header
 mv vimIDE_config/user.vimrc ~/.vimrc
 # 安装vim插件管理Vundle
-mkdir -p ~.vim/bundle
+mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # 执行安装vim插件
 vim +PluginInstall +qall
@@ -30,6 +30,7 @@ pip install jedi
 which pip3 && pip3 install jedi
 # pe8调整插件
 pip install autopep8
-
+# 用含vim插件配置的文件覆盖单独插件列表的文件
+mv -f vimIDE_config/vimrc_pymode.sh ~/.vimrc.bundles
 # 安装完成提示
 echo "恭喜你完成安装"
