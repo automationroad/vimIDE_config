23b19d1535c0ef20215ccb0a2e24b7e9e494eb1d
LAPTOP-6L7QMV7L\浪子尘心 <536418286@qq.com>
2018-10-04 17:39:36 +0800

    修改执行路径问题

--- second_version/deploy_vimIDE.sh
+++ second_version/deploy_vimIDE.sh
@@ -26,7 +26,7 @@ vim -c "PlugInstall" -c "q" -c "q"
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 # 完整的配置覆盖插件列表配置
-cd ~
+cd -
 cp -f vimIDE_config/second_version/.vimrc.bundles ~/.vimrc.bundles
 # 再执行一次，确保所有的插件安装
 vim -c "PlugInstall" -c "q" -c "q"
