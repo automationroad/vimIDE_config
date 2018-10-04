9bf281c5d682d51f62c1cc01ea187a76cf9abc50
LAPTOP-6L7QMV7L\浪子尘心 <536418286@qq.com>
2018-10-04 14:56:43 +0800

    修改文件名称

--- second_version/deploy_vimIDE.sh
+++ second_version/deploy_vimIDE.sh
@@ -5,7 +5,7 @@
 # Author: 浪子尘心
 # Mail: hj536418286@gmail.com
 # Created Time: 2018-10-04 14:41:10
-# Last Changed: 2018-10-04 14:48:10
+# Last Changed: 2018-10-04 14:54:31
 # Description: 更换插件管理器，主题设置
 # Version: 0.4
 #########################################################################
@@ -14,10 +14,10 @@
 [ -f ~/.vimrc ] && mv ~/.vimrc  ~/.vimrc.bak
 [ -d ~/.vim ] && mv ~/.vim ~/.vim_bak
 # 配置vim
-cp -f vimIDE_config/second/.vimrc ~/.vimrc
-cp -f vimIDE_config/second/.vimrc.bundles ~/.vimrc.bundles
+cp -f vimIDE_config/second_version/.vimrc ~/.vimrc
+cp -f vimIDE_config/second_version/.vimrc.bundles ~/.vimrc.bundles
 mkdir -p ~/.vim/vim_header_model
-cp -f vimIDE_config/second/vim_header ~/.vim/vim_header_model/vim_header
+cp -f vimIDE_config/second_version/vim_header ~/.vim/vim_header_model/vim_header
 # 安装vim插件
 vim -c "PlugInstall" -c "q" -c "q"
 # jedi 支持插件
